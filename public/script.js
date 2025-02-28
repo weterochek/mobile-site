8677fa641f3e260d7dad807a58b85c4fc2dc83a6
weterochek <132129419+weterochek@users.noreply.github.com>
2025-02-28 18:00:29 +0900

    Update script.js

--- public/script.js
+++ public/script.js
@@ -308,7 +308,7 @@ function editField(field) {
         input.disabled = false;
         input.focus();
     } else {
-        fetch("https://makadamia.onrender.com/account", {
+        fetch("https://mobile-site.onrender.com/account", {
             method: "PUT",
             headers: {
                 "Content-Type": "application/json",
