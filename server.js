0b52c62858a4d6509fc8ca5f7b49fe8d4560fab9
weterochek <132129419+weterochek@users.noreply.github.com>
2025-03-01 23:05:51 +0900

    Update server.js

--- server.js
+++ server.js
@@ -279,6 +279,7 @@ app.post('/login', async (req, res) => {
         httpOnly: true,
         secure: true,
         sameSite: "None",
+        domain: ".onrender.com",
         maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
@@ -330,6 +331,8 @@ app.post('/refresh', async (req, res) => {
             httpOnly: true,
             secure: true,
             sameSite: "None",
+            domain: ".onrender.com",
+            path: "/",
             maxAge: 30 * 24 * 60 * 60 * 1000,
         });
 
@@ -355,7 +358,9 @@ app.post('/logout', authMiddleware, (req, res) => {
     res.clearCookie(cookieName, {
         httpOnly: true,
         secure: true,
-        sameSite: 'None'
+        sameSite: 'None',
+        domain: ".onrender.com",
+        path: "/"
     });
 
     res.json({ message: 'Вы вышли из системы' });
