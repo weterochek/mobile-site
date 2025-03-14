d31b9cc9658035adc01b93f779fedb0b9dab7c19
weterochek <132129419+weterochek@users.noreply.github.com>
2025-03-14 16:18:08 +0900

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,1 +1,18 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  items: [
+    {
+      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+      quantity: { type: Number, required: true },
+    }
+  ],
+  address: { type: String, required: true },
+  additionalInfo: { type: String },
+  status: { type: String, enum: ["Оформлен", "Отправлен", "Доставлен"], default: "Оформлен" },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model("Order", OrderSchema);
 
