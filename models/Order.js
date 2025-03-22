05cd558807fbc9ebf1995335cd88da68ec880e38
weterochek <132129419+weterochek@users.noreply.github.com>
2025-03-22 21:33:55 +0900

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -1,18 +1,21 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
-const OrderSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
-  items: [
-    {
-      productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-      quantity: { type: Number, required: true },
-    }
-  ],
-  address: { type: String, required: true },
-  additionalInfo: { type: String },
-  status: { type: String, enum: ["Оформлен", "Отправлен", "Доставлен"], default: "Оформлен" },
-  createdAt: { type: Date, default: Date.now }
+const orderSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    deliveryTime: String,
+    name: { type: String, required: true },
+    address: { type: String, required: true },
+    additionalInfo: { type: String },
+    createdAt: { type: Date, default: Date.now },  // Время оформления заказа
+    items: [
+        {
+            productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product', required: true },
+            quantity: { type: Number, required: true },
+            price: { type: Number, required: true },
+        }
+    ],
+    totalAmount: { type: Number, required: true }  // Общая сумма
 });
 
-module.exports = mongoose.model("Order", OrderSchema);
-
+const Order = mongoose.model('Order', orderSchema);
+module.exports = Order;
