d3c1ad827e3e46e44e19ef107980db1f3a814221
weterochek <132129419+weterochek@users.noreply.github.com>
2025-03-22 21:33:28 +0900

    Update authMiddleware.js

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,17 +1,18 @@
-const jwt = require('jsonwebtoken');
-
-module.exports = (req, res, next) => {
-    const authHeader = req.headers.authorization;
-    if (!authHeader) {
-        return res.status(401).json({ message: "Токен не предоставлен" });
-    }
-
-    const token = authHeader.split(' ')[1];
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET || "your_jwt_secret");
-        req.user = decoded;  // Извлекаем данные пользователя
-        next();
-    } catch (err) {
-        return res.status(401).json({ message: "Недействительный токен" });
-    }
-};
+const jwt = require("jsonwebtoken");
+ const JWT_SECRET = process.env.JWT_SECRET || "ai3ohPh3Aiy9eeThoh8caaM9voh5Aezaenai0Fae2Pahsh2Iexu7Qu/";
+ 
+ module.exports = (req, res, next) => {
+     const authHeader = req.headers.authorization;
+     if (!authHeader) {
+         return res.status(401).json({ message: "Токен не предоставлен" });
+     }
+ 
+     const token = authHeader.split(" ")[1];
+     try {
+         const decoded = jwt.verify(token, JWT_SECRET);
+         req.user = decoded;
+         next();
+     } catch (err) {
+         return res.status(401).json({ message: "Недействительный токен" });
+     }
+ };
