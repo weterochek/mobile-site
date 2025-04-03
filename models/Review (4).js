92a1e1c1ef9e5b2dce75d760306af75efbd2fe1f
weterochek <132129419+weterochek@users.noreply.github.com>
2025-04-04 03:05:06 +0900

    Add files via upload

---
+++ models/Review (4).js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema({
+    rating: {
+        type: Number,
+        required: true,
+        min: 1,
+        max: 5
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+    username: {
+        type: String,
+        required: true
+    },
+    displayName: {
+        type: String,
+        default: null
+    },
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model("Review", reviewSchema);
+
