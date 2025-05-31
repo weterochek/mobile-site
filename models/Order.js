b5058255c694af95f4cfef6313953658c8ca85f1
weterochek <132129419+weterochek@users.noreply.github.com>
2025-05-31 16:10:55 +0900

    Update Order.js

--- models/Order.js
+++ models/Order.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const orderSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
     deliveryTime: String,
+    phone: { type: String, required: true },
     name: { type: String, required: true },
     address: { type: String, required: true },
     additionalInfo: { type: String },
