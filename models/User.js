fd92d1302a83d794ec7bfdc2efebf65218b9e82b
weterochek <132129419+weterochek@users.noreply.github.com>
2025-06-02 19:22:43 +0900

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,7 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  resetToken: String,
+  resetTokenExpiration: Date,
   name: { type: String, default: "" },
   city: { type: String, default: "" },
   orders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }]  // Связь с заказами
